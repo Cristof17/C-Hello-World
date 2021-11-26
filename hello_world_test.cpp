bd22ea9d164559903b278ff0121a173e42550e6e
Cristof <cristof@Cristofs-MacBook-Air.local>
2021-11-26 12:48:31 +0200

    first commit

---
+++ hello_world_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string initialString = "Hello world";
+    string initialization = ("Hello World");
+    cout << initialization << endl;
+    return 0;
+}
+        
